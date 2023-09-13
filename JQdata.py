043ff59f2734f21a16c501089b8389bd72fce8c2
Liuchuwei <liuchw3@mail2.sysu.edu.cn>
2023-09-13 17:39:22 +0800

    AKshare

--- JQdata.py
+++ JQdata.py
@@ -1,3 +1,8 @@
 from jqdatasdk import *
 
+# Authentication
 auth('13600081563','Liuchuwei1357')
+
+df=get_price('000001.XSHE',count=2,end_date='2023-01-31',frequency='daily',
+                   fields=['open', 'close'],panel = False)
+print(df)
